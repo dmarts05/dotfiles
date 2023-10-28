0f5a4dd4c88d05fa2909240878e321af289d6c92
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-10-28 22:43:31 +0200

    feat: improve Desktop autostart script

--- X11/Desktop/.config/awesome/autostart.sh
+++ X11/Desktop/.config/awesome/autostart.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Kill all systray icons, polkit and lock
-sleep 1 && (killall pa-applet && killall nm-applet && killall polkit-gnome-authentication-agent-1 && killall xss-lock)
+killall pa-applet && killall nm-applet && killall polkit-gnome-authentication-agent-1 && killall xss-lock
 
 # Start systray icons, polkit and lock
-sleep 2 && (pa-applet & nm-applet --indicator & /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & xss-lock i3lock &)
+sleep 0.5 && (pa-applet & nm-applet --indicator & /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & xss-lock i3lock &)
+
+# Rerun pa-applet if it crashes
+sleep 3 && if ! pgrep -x "pa-applet" > /dev/null; then pa-applet & fi
