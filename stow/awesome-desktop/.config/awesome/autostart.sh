07980d19a5fff7c45fe5e09d29a10cd9541631be
dmarts05 <dmarts05@estudiantes.unileon.es>
2025-06-09 19:18:31 +0200

    feat: add dual monitor setup for desktop

--- stow/awesome-desktop/.config/awesome/autostart.sh
+++ stow/awesome-desktop/.config/awesome/autostart.sh
@@ -9,3 +9,12 @@ run() {
 run "/usr/lib/xfce-polkit/xfce-polkit"
 run "nm-applet" --indicator
 run "xss-lock" -- "i3lock" -c "#1e1e2e"
+
+xrandr \
+  --output DP-0 --off \
+  --output DP-1 --off \
+  --output DP-2 --primary --mode 1920x1080 --rate 170 --pos 0x0 --rotate normal \
+  --output DP-3 --off \
+  --output HDMI-0 --mode 1920x1080 --pos 1920x106 --rotate normal \
+  --output DP-4 --off \
+  --output DP-5 --off
